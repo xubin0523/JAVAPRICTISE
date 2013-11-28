500f6aac6f2f0f63b25e1dd7c6499e7d99976f08
imyours <xubin0523@gmail.com>
2013-11-28 14:44:12 +0800

    test

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mark.xub
+ * Date: 13-11-28
+ * Time: 下午1:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewClass {
+    public List<String> list;
+    public static void main(String[] args) {
+             sdfsdfas  ksdjfkdsfjks
+                     ksdfjkdf
+                             ksdfjkdf
+    }
+}
